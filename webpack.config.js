dac24533e7361490d320a9c462389ac1a06220d2
Michael West <michaelwest299@gmail.com>
2016-10-23 17:50:58 +0100

    video 5 progress

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
 var HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
   template: __dirname + '/app/index.html',
   filename: 'index.html',
   inject: 'body'
 });
+
 module.exports = {
   entry: [
     './app/index.js'
